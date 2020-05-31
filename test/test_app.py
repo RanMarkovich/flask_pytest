9c2bdd46a767825982f078f8b65f2c75ce58e3ff
Ran <ran@minutemedia.com>
2020-05-31 21:36:24 +0300

    localhost

--- test/test_app.py
+++ test/test_app.py
@@ -5,6 +5,6 @@ import requests
 
 def test_hello_flask():
     # time.sleep(3600)
-    r = requests.get('http://127.0.0.1:5000')
+    r = requests.get('http://localhost:5000')
     assert r.status_code == 200, f"expected status code 200, instead got: {r.text}"
     assert r.text == 'hello_flask', f"expected 'hello_flask' in response text, instead got: {r.text}"
